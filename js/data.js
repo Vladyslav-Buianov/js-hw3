273985cb90d5882782707af4dca915feb51430f1
Vladyslav <vitbuianov.20130225@gmail.com>
2026-01-29 17:06:31 +0100

    first commit

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const result= 5 + 5 + '5';
+console.log(result);
+const email = 'vitbuianov.20130225@gmail.com';
+console.log(`${email.includes('@')} ${email.length}`);
+const posesive = 'My';
+const subject = 'name';
+const verb = 'is';
+const fullName = posesive + ' ' + subject + ' ' + verb + ' Viktor'
+console.log(fullName);
+const userName = 'Владислав';
+const payment = 1500;
+alert(`Дякуємо, ${userName} ! До сплати ${payment}$.`);
